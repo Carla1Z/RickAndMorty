2d257a786dd99332b12c0b96c126716dfa3d1d73
Carla1Z <carla_sabrina_zarate@hotmail.com>
2022-09-19 16:48:33 -0300

    first commit

---
+++ api/index.js
@@ -0,0 +1,12 @@
+const server = require("./src/app.js");
+const { conn } = require("./src/db.js");
+
+// Syncing all the models at once.
+
+// Para la precarga cuando se levanta el server, ejecutar la funcion getEpisodes(). Al ser una peticion vamos a usar async await.
+
+conn.sync({ force: true }).then(() => {
+  server.listen(3001, () => {
+    console.log("Listening at 3001"); // eslint-disable-line no-console
+  });
+});
