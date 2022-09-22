6c45653402a430c75115ff98965820a4595b7668
Carla1Z <carla_sabrina_zarate@hotmail.com>
2022-09-22 15:35:29 -0300

    searchbar css

--- client/src/components/Searchbar/index.jsx
+++ client/src/components/Searchbar/index.jsx
@@ -1,10 +1,15 @@
 import styles from "./SearchBar.module.css";
+import lupa from "../../assets/lupa.png";
 
 function Searchbar() {
   return (
-    <div className={styles.searchbar}>
-      <input type="text" className={styles.search} />
-      <input type="submit" className={styles.button} />
+    <div>
+      <form className={styles.form}>
+        <input type="text" className={styles.input} />
+        <button type="submit" className={styles.button}>
+          <img src={lupa} />
+        </button>
+      </form>
     </div>
   );
 }
