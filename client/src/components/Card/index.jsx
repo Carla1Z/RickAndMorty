c8b7e2fab63ed28be01062d36e4d04d21a85d01e
Carla1Z <carla_sabrina_zarate@hotmail.com>
2022-09-29 14:56:07 -0300

    card animaciones

--- client/src/components/Card/index.jsx
+++ client/src/components/Card/index.jsx
@@ -3,11 +3,30 @@ import styles from "./Card.module.css";
 function Card({ image, name, origin, species, episode }) {
   return (
     <div className={styles.card}>
-      <h3>{name}</h3>
-      <img src={image} alt="character" />
-      <p>{origin}</p>
-      <p>{species}</p>
-      <p>{episode}</p>
+      {/* <div className={styles.face}>
+        <div className={styles.front}>
+          <h3>{name}</h3>
+          </div>
+          
+          <div className={styles.back}>
+          <p>{origin}</p>
+          <p>{species}</p>
+          <p>{episode}</p>
+          </div>
+        </div> */}
+
+      <div className={styles.front}>
+        <img src={image} alt="character" />
+      </div>
+
+      <div className={styles.back}>
+        <h3>{name}</h3>
+        <ul>
+          <li>{origin}</li>
+          <li>{species}</li>
+          <li>{episode}</li>
+        </ul>
+      </div>
     </div>
   );
 }
