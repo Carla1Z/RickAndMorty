70a0313cb5bc4c47005a29ae03bf3bc1b418e293
Carla1Z <carla_sabrina_zarate@hotmail.com>
2022-10-04 20:50:45 -0300

    create

--- client/src/components/Create/index.jsx
+++ client/src/components/Create/index.jsx
@@ -10,9 +10,36 @@ function Create() {
     origin: "",
     species: "",
     image: "",
+    episodes: [],
   });
 
-  const [errorButton, setErrorButton] = useState(true)
+  const [errorButton, setErrorButton] = useState(true);
+
+  const [errorForm, setErrorForm] = useState({});
+
+  //Funcion para completar los cambios --> avisa errores,validar
+  //Funcion para enviar el formulario ---> sin errores, previamente validado
+
+  const handleChange = (e) => {
+    setCreate({
+      ...create,
+      [e.target.name]: e.target.value,
+    });
+    //↑↑ asigno valor
+    //↓↓ valido
+    // validate(create)
+    setErrorForm(validate(create));
+    console.log(create);
+  };
+
+  const handleEpisodes = (e) => {
+    setCreate({
+      ...create,
+      //new Set elimina los valores repetidos
+      episodes: [...new Set([...create.episodes, e.target.value])],
+      // episodes: Array.from(new Set([...create.episodes, e.target.value])),
+    });
+  };
 
   const handleSubmit = (e) => {
     e.preventDefault();
@@ -22,8 +49,25 @@ function Create() {
       origin: "",
       species: "",
       image: "",
-    })
+      episodes: [],
+    });
   };
+
+  const validate = (info) => {
+    let errores = {};
+    if (validName(info.name)) errores.name = "Hay errores en el nombre";
+    if (!info.species || typeof info.species !== "string")
+      errores.species = "Hay errores en especie";
+
+    return errores;
+    //el objeto de errores puede estar vacio o tener propiedades
+    //si contiene propiedades es que hay errores en el codigo
+  };
+
+  function validName(str) {
+    if (typeof str !== "string") return true;
+    if (str.length < 3) return true;
+  }
 
   return (
     <div className={styles.create}>
@@ -31,29 +75,76 @@ function Create() {
         <label>
           <span>Nombre: </span>
           <input
+            name="name"
+            onChange={handleChange}
             type="text"
             value={create.name}
             placeholder="Nombre de tu create"
           />
+          {errorForm.name ? (
+            <h4>
+              <small>{errorForm.name}</small>
+            </h4>
+          ) : (
+            false
+          )}
         </label>
         <label>
           <span>Origen: </span>
-          <input type="text" value={create.origin} placeholder="..." />
+          <input
+            name="origin"
+            onChange={handleChange}
+            type="text"
+            value={create.origin}
+            placeholder="..."
+          />
         </label>
         <label>
           <span>Especie: </span>
-          <input type="text" value={create.species} placeholder="..." />
+          <input
+            name="species"
+            onChange={handleChange}
+            type="text"
+            value={create.species}
+            placeholder="..."
+          />
+          {errorForm.species ? (
+            <h4>
+              <small>{errorForm.species}</small>
+            </h4>
+          ) : (
+            false
+          )}
         </label>
         <label>
           <span>Imágen: </span>
           <input
+            name="image"
+            onChange={handleChange}
             type="text"
             value={create.image}
             placeholder="http://wwww.example.com/"
           />
         </label>
         <label>
-          <input type="submit" value="Crear" className={styles.button} />
+          <span>Episodios: </span>
+          <select
+            name="episodes"
+            value={create.episodes}
+            onChange={handleEpisodes}
+            required
+          >
+            <option value={1}>Episodio 1</option>
+            <option value={2}>Episodio 2</option>
+          </select>
+        </label>
+        <label>
+          <input
+            type="submit"
+            value="Crear"
+            disabled={errorButton ? true : false}
+            // className={styles.button}
+          />
         </label>
       </form>
     </div>
