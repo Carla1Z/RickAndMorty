8979296814033830ea9978062e42e3a16e19f00b
Carla1Z <carla_sabrina_zarate@hotmail.com>
2022-12-06 16:02:08 -0300

    se guarda en db los episodios

--- api/src/controllers/allEpisodesInfo.js
+++ api/src/controllers/allEpisodesInfo.js
@@ -2,34 +2,20 @@ const axios = require("axios");
 const { Episode } = require("../db");
 
 const allEpisodes = async () => {
-  let episodes = [];
-  for (let i = 1; i < 4; i++) {
-    const apiData = await axios.get(
-      `https://rickandmortyapi.com/api/episode?page=${i}`
-    );
+  try {
+    let episodes = [];
+    for (let i = 1; i < 4; i++) {
+      const apiData = await axios.get(
+        `https://rickandmortyapi.com/api/episode?page=${i}`
+      );
 
-    episodes = episodes.concat(apiData.data.results);
-
-    episodes = episodes.map((el) => el.name);
-    console.log(episodes);
-
-    let episodesEach = episodes.forEach((el) => {
-      Episode.findOrCreate({
-        where: { name: el.name },
-      });
-    });
-    return episodesEach;
+      episodes = episodes.concat(apiData.data.results);
+    }
+    await Episode.bulkCreate(episodes);
+    console.log("Se guarda en db los episodios");
+  } catch (error) {
+    console.log("Error en allEpisodes: ", error);
   }
 };
-
-// episodes = episodes.concat(apiData.data.results);
-// let episodesMap = episodes.map((el) => el.name);
-// console.log(episodesMap);
-// let episodesEach = episodes.forEach((el) => {
-//   Episode.findOrCreate({
-//     where: { name: el.name },
-//   });
-// });
-// return episodesEach;
 
 module.exports = allEpisodes;
